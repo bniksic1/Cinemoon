9d2cbf3c4ec6b475840574d26d6ef6104bc4d9e1
bniksic1 <bniksic1@etf.unsa.ba>
2021-02-26 23:38:20 +0100

    initial commit

---
+++ src/test/java/com/github/bniksic1/CinemoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bniksic1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
